547bbb6909e48d78be82f2749b6731d993110899
Hilal <hilalyilmaz016@gmail.com>
2023-07-22 17:48:55 +0300

    tamamlandı

--- ip/index.js
+++ ip/index.js
@@ -1,4 +1,5 @@
 //axios import buraya gelecek
+import axios from "axios";
 
 var benimIP;
 
@@ -20,6 +21,9 @@ async function ipAdresimiAl(){
 	});
 }				
 // ------------ değiştirmeyin --------------
+
+
+// Make a request for a user with a given ID
 
 
 /*
@@ -54,10 +58,100 @@ async function ipAdresimiAl(){
     </div>
 */
 
+const cardYapici = (data)=>{
+	const card = document.createElement("div");
+	card.classList.add("card");
+
+	const bayrak = document.createElement("img");
+	bayrak.setAttribute("src", data?.["ülkebayrağı"]);
+	card.append(bayrak);
+
+	const cardInf= document.createElement("div");
+	cardInf.classList.add("card-info");
+	card.append(cardInf);
+
+	const baslik = document.createElement("h3");
+	baslik.classList.add("ip");
+	baslik.textContent= `IP: ${data?.sorgu}`;
+	cardInf.append(baslik);
+
+	const ulkeBl= document.createElement("p");
+	ulkeBl.classList.add("ulke");
+	ulkeBl.textContent=`${data?.["ülke"]} (${data?.["ülkeKodu"]})`;
+	cardInf.append(ulkeBl);
+
+	const enlBoyl = document.createElement("p");
+	enlBoyl.textContent= `Enlem:${data?.enlem} Boylam:${data?.boylam}`;
+	cardInf.append(enlBoyl);
+
+	const sehirBlg= document.createElement("p");
+	sehirBlg.textContent=`Şehir: ${data?.bölgeAdı}`;
+	cardInf.append(sehirBlg);
+
+	const saatDilimi= document.createElement("p");
+	saatDilimi.textContent= `Saat dilimi: ${data?.saatdilimi}`;
+	cardInf.append(saatDilimi);
+
+	const parBir = document.createElement("p");
+	parBir.textContent=`Para birimi: ${data?.parabirimi}`;
+	cardInf.append(parBir);
+
+	const isp= document.createElement("p");
+	isp.textContent=`ISP: ${data?.isp}`;
+	cardInf.append(isp);
+
+	return card;
+
+
+}
+
+const cardContainer= document.querySelector(".cards");
+let myIp= null;
+async function getMyIp (){
+	
+	await axios
+	  .get("https://apis.ergineer.com/ipadresim")
+	  .then(function (response) {
+		myIp=response.data;
+		return myIp;
+		// handle success
+		
+	  })
+	  .catch(function (error) {
+		// handle error
+		console.log(error);
+	  })
+	  .finally(function () {
+		console.log("myIp" ,myIp)
+		// always executed
+	  });
+}
+
+async function getApi (){
+	await getMyIp();
+	
+	axios
+	  .get(`https://apis.ergineer.com/ipgeoapi/${myIp}`)
+	  .then(function (response) {
+		
+		cardContainer.append(cardYapici(response.data));
+		// handle success
+		
+	  })
+	  .catch(function (error) {
+		// handle error
+		console.log(error);
+	  })
+	  
+}
+ getApi();
+console.log("aaa", getApi());
+
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
 	bu kartı DOM olarak .cards elementinin içine ekleyin. 
 */
+
 
 /*
 	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün. 
